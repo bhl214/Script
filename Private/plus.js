e67058a290c4029aaeac72bc408b64630ec3507b
None <6121056+nobodynone@users.noreply.github.com>
2019-12-04 22:02:58 +0800

    add

--- Private/plus.js
+++ Private/plus.js
@@ -1,4 +1,16 @@
-var obj = JSON.parse($response.body);
+
+let body= $response.body;
+var obj = JSON.parse(body);
+if (body.indexOf("expires") !=-1) {
+  obj["receipt"]["in_app"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
+  obj["receipt"]["in_app"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
+  obj["receipt"]["in_app"][0]["expires_date_ms"] = "4096019658000";
+  obj["latest_receipt_info"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
+  obj["latest_receipt_info"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
+  obj["latest_receipt_info"][0]["expires_date_ms"] = "4096019658000";
+  }
+$done({body: JSON.stringify(obj)});
+
 var bundle_id = obj.receipt["bundle_id"];
 if (bundle_id == "net.shinyfrog.bear-iOS") {
   obj = {
